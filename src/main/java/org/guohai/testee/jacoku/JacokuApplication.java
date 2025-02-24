2c9c47ed731be27454b024d84755e7f1cf2bd602
guohai <guohai@gyyx.cn>
2025-02-24 19:30:16 +0800

    feat: init project

---
+++ src/main/java/org/guohai/testee/jacoku/JacokuApplication.java
@@ -0,0 +1,13 @@
+package org.guohai.testee.jacoku;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JacokuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JacokuApplication.class, args);
+	}
+
+}
